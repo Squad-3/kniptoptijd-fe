9c693100791552268db2485f2d984d67ab6a86fb
Mitchell John <mitchellvdhut@gmail.com>
2020-11-27 01:02:10 +0100

    Overview layout

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -97,49 +98,65 @@ class _MyHomePageState extends State<MyHomePage> {
     // fast, so that you can just rebuild anything that needs updating rather
     // than having to individually change instances of widgets.
     return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Container (
-          color :Colors.grey[100],
-          child: Column(
-            // Column is also a layout widget. It takes a list of children and
-            // arranges them vertically. By default, it sizes itself to fit its
-            // children horizontally, and tries to be as tall as its parent.
-            //
-            // Invoke "debug painting" (press "p" in the console, choose the
-            // "Toggle Debug Paint" action from the Flutter Inspector in Android
-            // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-            // to see the wireframe for each widget.
-            //
-            // Column has various properties to control how it sizes itself and
-            // how it positions its children. Here we use mainAxisAlignment to
-            // center the children vertically; the main axis here is the vertical
-            // axis because Columns are vertical (the cross axis would be
-            // horizontal).
-            mainAxisAlignment: MainAxisAlignment.center,
-              children: <Widget>[
-                Text(
-                  'You have clicked the button this many times:',
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: listCount,
-                    itemBuilder: (context, i) {
-                      return ListTile(
-                          title: Text('Kapper id: ' + _counter[i].toString())
-                      );
-                    },
-                  )
-                )
-              ],
+      backgroundColor: Colors.transparent,
+      // appBar: PreferredSize(
+      //   preferredSize: Size.fromHeight(150.0),
+      //   child: AppBar(
+      //     // Here we take the value from the MyHomePage object that was created by
+      //     // the App.build method, and use it to set our appbar title.
+      //     title: Text(widget.title),
+      //     elevation: 0,
+      //     backgroundColor: Colors.transparent,
+      //     centerTitle: true,
+      //   ),
+      // ),
+      body: Stack(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              image: DecorationImage(
+                image: AssetImage("assets/images/scaffold-bg.png"),
+                fit: BoxFit.contain,
+                alignment: Alignment(-1.0, -1.0)
+              ),
+            ),
           ),
-        ),
+          Container(
+            alignment: Alignment.center,
+            padding: EdgeInsets.only(top: 80),
+            child: FractionallySizedBox(
+              widthFactor: 0.9,
+              child: Container (
+                padding: EdgeInsets.all(20),
+                decoration: BoxDecoration(
+                  color :Colors.white,
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    Text(
+                      'Zoekresultaten voor:',
+                      style: TextStyle(fontWeight: FontWeight.bold, fontSize: 20.0),
+                    ),
+                    Text('1234AB, Amsterdam om 12 uur'),
+                    Expanded(
+                      child: ListView.builder(
+                        itemCount: listCount,
+                        itemBuilder: (context, i) {
+                          return ListTile(
+                            title: Text('Kapper id: ' + _counter[i].toString(),)
+                          );
+                        },
+                      )
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ),
+        ]
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
