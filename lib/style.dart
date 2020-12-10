4468a2b242b45f62661efd70903111ff53166c0d
Mitchell John <mitchellvdhut@gmail.com>
2020-12-10 11:08:27 +0100

    Screens samengevoegd

---
+++ lib/style.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+
+const LargeTextSize = 25.0;
+const MediumTextSize = 20.0;
+const BodyTextSize = 16.0;
+
+const String TitleFont = 'Quicksand';
+
+const AppBarTextStyle = TextStyle(
+  fontFamily: TitleFont,
+  fontWeight: FontWeight.w300,
+  fontSize: MediumTextSize,
+  color: Colors.black,
+);
+
+const MainTitleStyle = TextStyle(
+    fontFamily: TitleFont,
+    fontWeight: FontWeight.w600,
+    fontSize: LargeTextSize,
+    color: Colors.white,
+    letterSpacing: 5.0);
+
+const Body1TextStyle = TextStyle(
+  fontFamily: TitleFont,
+  fontWeight: FontWeight.w300,
+  fontSize: BodyTextSize,
+  color: Colors.white,
+);
